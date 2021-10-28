d40daeba5285072f15e66f5fc244f4338a314c14
Kamil Synowiec <kamilsynowiec2@gmail.com>
2021-10-28 19:03:30 +0100

    Serve an html file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+/*
 app.get("/", function(req, res){
     res.send("Hello Express");
-})
+});
+*/
 
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/views/index.html");
+});
 
 
 
